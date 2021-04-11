802179abbd5597e92e5cf0b06752944a8468245c
LeVi <56607973+LeViCantCode@users.noreply.github.com>
2021-04-10 18:44:26 -0700

    Update SycadaIndex.js

--- SycadaIndex.js
+++ SycadaIndex.js
@@ -1,71 +1,70 @@
-const Discord = require("discord.js"),
-client = new Discord.Client();
-const cfg = require("./config.json");
+const Discord = require('discord.js');
 const fs = require('fs');
-const vosk = require('vosk')
-const model = new vosk.Model("./vosk-model-en-us-aspire-0.2");
-client.commands = new Discord.Collection();
-
-let commandFiles = fs.readdirSync('./Commands').filter(file => file.endsWith('.js'));
-commandFiles.forEach(File => {
-    let command = require(`./Commands/${File}`);
-    command.Path = (`${process.cwd()}/Commands/${File}`);
-    client.commands.set(command.name, command);
-    if (command.aliases) {
-        command.aliases.forEach(Name => {
-            client.commands.set(Name, command);
-        })
-    }
+const ffmpeg = require('fluent-ffmpeg');
+const client = new Discord.Client(); //main client
+const { Readable } = require('stream'); //streams
+const { OpusEncoder } = require('@discordjs/opus');
+const { IamAuthenticator } = require('ibm-watson/auth');
+const SpeechToTextV1 = require('ibm-watson/speech-to-text/v1');
+let speech_to_text = new SpeechToTextV1({
+    authenticator: new IamAuthenticator({
+        apikey: 'REMOVED, PRIVATE INFO',
+    }),
+    serviceUrl: 'REMOVED, PRIVATE INFO'
 });
-client.on('ready', () => {
-    console.log(
-        `Bot has started, in ${client.guilds.cache.size} servers.`
-    );
-    client.user.setPresence({
-        status: 'online',
-        activity: {
-            name: 'Your servers.',
-            type: 'WATCHING'
+
+async function doSpeech(msg) {
+    let connection = await msg.member.voice.channel.join()
+    if (connection) msg.channel.send("successfully started speech to text/already in VC")
+    let stream;
+
+    connection.on('speaking', (user, speaking) => {
+        if (speaking.bitfield == 0 || user.bot)
+            return
+
+        let audio = connection.receiver.createStream(user, { mode: 'pcm', end: 'silence' });
+
+        if (speaking) {
+            console.log(`Speaking: ${user.tag}`);
+            let talking = user.username
+            stream = ffmpeg(audio).fromFormat('s32le').toFormat('wav').pipe();
+
+            stream.pipe(speech_to_text.recognizeUsingWebSocket({ objectMode: true }).on('error', e => console.log(e)).on('data', (d) => {
+                let result = d.results.map(result => result.alternatives.map(alternative => alternative.transcript).join(' ')).join(' ');
+                if (result.length > 1 && result !== '%HESIATION')
+                    msg.channel.send(`${talking}: ${result.replace(/%HESITATION/g, '')}`);
+                
+            }));
         }
     });
-});
+}
 
-client.on('message', msg => {
-    if (msg.content.includes(`<@!${client.user.id}>`)) {
-        msg.reply(
-            `My prefix is ${cfg.Prefix} to find out more do ${cfg.Prefix}help`
-        );
-    }
-    if (!msg.content.startsWith(cfg.Prefix) || msg.author.bot) return;
-    let args = msg.content.slice(cfg.Prefix.length).trim().split(/ +/g);
-    let command = args.shift().toLowerCase();
-    let LVL = cfg.Admin.some(Elm => msg.member.roles.cache.has(Elm)) || msg.member.hasPermission('MANAGE_CHANNELS') ? 1 : 0;
-    LVL = cfg.Dev.includes(msg.author.id) ? 2 : LVL;
-    if (client.commands.has(command)) {
-        let Permit;
-        switch (client.commands.get(command).Level) {
-            case 'Dev':
-                Permit = 2;
-                break;
-            case 'Admin': 
-                Permit = 1;
-                break;
-            case 'User':
-                Permit = 0;
-                break;
-            default:
-                Permit = 0;
-                break;
-        }
-        if (LVL >= Permit) {
-            try {
-                client.commands.get(command).execute(msg, args, client, Discord, cfg.Prefix);
-            } catch (error) {
-                console.log(error);
-                msg.reply('There was an error trying to execute that command!');
+client.on('ready', () => console.log('Ready'))
+
+client.on('message', async msg => {
+    if (msg.author.bot) return;
+    if (!msg.content.startsWith('!')) return;
+    let command = msg.content.slice(1).trim();
+    switch (command) {
+        case 'leave':
+            if (msg.guild.voice.channel != null || msg.guild.voice.channel != undefined) {
+                msg.guild.voice.channel.leave();
+                msg.channel.send("I have successfully left the voice channel!");
+            } else msg.channel.send("I'm not connected to a voice channel!");
+            break;
+        case 'speech':
+            if (!msg.member.voice.channel) {
+                msg.channel.send("You are not in a VC please join one and use !speech again")
+                return;
             }
-        } else msg.reply('You do not have The Perms To use this command');
+            doSpeech(msg);
+            break;
+        case 'tts':
+            // connection
+            break;
+        case 'help':
+            msg.channel.send("Nothing fancy because I'm lazy right now, we currently have !speech to enable speech to text and !leave to get the bot to leave the voice channel")
+            break;
     }
 });
-
-client.login(process.env.TOKEN);
+client.login('REMOVED, PRIVATE INFO');
